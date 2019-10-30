bf69b917c7f39617b012e2ffb1ea41be8e116e42
Conzel <38732545+Conzel@users.noreply.github.com>
2019-10-30 21:46:00 +0100

    ADD: Multiple Survey Support

--- application.py
+++ application.py
@@ -11,14 +11,16 @@ application.add_url_rule(
     '/', 'index', (lambda: "Please append /gen_numbers to generate numbers or /pop_number to receive a random, unique user id"))
 
 # prepares s3 bucket
+
+
 def getBucket():
     s3 = boto3.resource("s3")
     return s3.Bucket("elasticbeanstalk-us-east-2-046856019993")
 
 # call with start and end integer. Produces permutated array and writes it to numbers.txt file
 # includes start and end
-@application.route("/gen_numbers/<start>/<end>")
-def gen_numbers(start, end):
+@application.route("/gen_numbers/<start>/<end>/<unique_identifier>")
+def gen_numbers(start, end, unique_identifier):
     # sanity checks
     # checks for abnormally large range
     range_size = abs(int(start) - int(end))
@@ -36,18 +38,21 @@ def gen_numbers(start, end):
     perm = list(range(int(start), int(end) + 1))
     shuffle(perm)
     text = "".join([str(x) + "\n" for x in perm])
-    bucket.put_object(Key="numbers.txt", Body=text)
+    bucket.put_object(Key="numbers" + unique_identifier + ".txt", Body=text)
     return "Generated numbers from " + start + " to " + end + ". You can visit /pop_number to verify it is working (caution, this of course invalidates one id)."
+
 
 @application.route("/gen_numbers")
 def hint_gen_numbers():
-    return "Hint: You probably try to generate numbers. Please append at the end of your path \"/start/end\", replacing with your desired start and end range for user IDs."
+    return "Hint: You probably try to generate numbers. Please append at the end of your path \"/start/end/unique_identifier\", replacing with your desired start and end range for user IDs. Please also append a unique identifier. This is used to make multiple surveys run in parallel."
 
-@application.route("/pop_number_debug")
-def pop_number():
+
+@application.route("/pop_number_debug/<unique_identifier>")
+def pop_number(unique_identifier):
+    file_name = "numbers" + unique_identifier + ".txt"
     bucket = getBucket()
     for obj in bucket.objects.all():
-        if obj.key == "numbers.txt":
+        if obj.key == file_name:
             numbers = obj.get()["Body"].read().split()
             # aws returns us bytes
             numbers = [int(x) for x in numbers]
@@ -61,20 +66,27 @@ def pop_number():
     else:
         rest = []
 
-    bucket.put_object(Key="numbers.txt", Body="\n".join(rest))
+    bucket.put_object(Key=file_name, Body="\n".join(rest))
 
     return str(return_number)
 
-@application.route('/pop_number', methods=['GET'])
-def api_hello():
+
+@application.route('/pop_number/<unique_identifier>', methods=['GET'])
+def pop_number_uri(unique_identifier):
     data = {
-        'user_id': pop_number(),
+        'user_id': pop_number(unique_identifier),
     }
     js = json.dumps(data)
 
     resp = Response(js, status=200, mimetype='application/json')
 
     return resp
+
+
+@application.route('/pop_number', methods=['GET'])
+def pop_number_hint():
+    return "You probably tried to pop a number. Please append the unique identifier of the ressource you wanted to pop. Example: Append /abc to pop the ressource with the identifier abc"
+
 
 # run the app.
 if __name__ == "__main__":
